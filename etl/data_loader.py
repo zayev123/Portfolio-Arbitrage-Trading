029806b1e5c846827ce9b039c4d37ef0610dbd86
Mir <mirbilal@Muneebs-MacBook-Pro.local>
2024-02-03 22:25:31 +0500

    ins1

--- etl/data_loader.py
+++ etl/data_loader.py
@@ -1,3 +1,4 @@
+import time
 from sqlalchemy import create_engine
 from db_constants import Base
 from sqlalchemy import func
@@ -5,12 +6,103 @@ import pandas as pd
 from models import ParentPair, ChildPair
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import String
+import queue
 
 class DataLoader:
     def __init__(self):
         engine = create_engine('sqlite:///Cryptos.db', echo=False)
         Session = sessionmaker(bind=engine)
         self.session = Session()
+        self.queue = queue.Queue()
+        self.time_to_stop = None
+
+    
+    def queue_init_data(self, parent_pair, symbol):
+        item = {
+            "type": "init",
+            "parent_pair": parent_pair,
+            "symbol": symbol
+        }
+        self.queue.put(item)
+    
+    def store_init_data(self, parent_pair, symbol):
+        parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==parent_pair).first()
+        if not parent_pair_obj:
+            parent_pair_obj = ParentPair(symbol=parent_pair)
+            self.session.add(parent_pair_obj)
+            self.session.commit()
+            parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==parent_pair).first()
+        parent_pair_obj = parent_pair_obj
+        
+        child_pair_obj = self.session.query(ChildPair).filter(
+            (ChildPair.symbol==symbol)
+            & (ChildPair.parent_pair_id==parent_pair_obj.id)
+        ).first()
+        if not child_pair_obj:
+            child_pair_obj = ChildPair(
+                symbol=symbol,
+                parent_pair_id = parent_pair_obj.id,
+            )
+            self.session.add(child_pair_obj)
+            self.session.commit()
+            child_pair_obj = self.session.query(ChildPair).filter(
+                (ChildPair.symbol==symbol)
+                & (ChildPair.parent_pair_id==parent_pair_obj.id)
+            ).first()
+        child_pair_obj = child_pair_obj
+        return child_pair_obj
+
+    def queue_latest_data(self, parent_pair, symbol, update):
+        item = {
+            "type": "update",
+            "parent_pair": parent_pair,
+            "symbol": symbol,
+            "update": update,
+        }
+        self.queue.put(item)
+
+    def store_latest_data(self, parent_pair, symbol, update):
+        best_bid_price = float(update['b'])
+        best_ask_price = float(update['a'])
+        
+        child_pair_obj = self.store_init_data(parent_pair, symbol)
+
+        if child_pair_obj:
+            child_pair_obj.best_bid_price = best_bid_price
+            child_pair_obj.best_ask_price = best_ask_price
+            self.session.add(child_pair_obj)
+            self.session.commit()
+
+    def process_items(self):
+        while True:
+            item: dict = self.queue.get()
+            if self.time_to_stop is not None:
+                print("time_to_stop", self.queue.qsize())
+            
+            item_type = item.get("type", None)
+            
+            if item_type == "init":
+                parent_pair = item.get("parent_pair", None)
+                symbol = item.get("symbol", None)
+                self.store_init_data(parent_pair=parent_pair, symbol=symbol)
+            
+            elif item_type == "update":
+                update = item.get("update", {})
+                parent_pair = item.get("parent_pair", None)
+                symbol = item.get("symbol", None)
+                self.store_latest_data(parent_pair=parent_pair, symbol=symbol,update=update)
+            
+            elif item_type == "stop":
+                break
+            
+            self.queue.task_done()
+
+    def stop_queue_process(self):
+        item = {
+            "type": "stop",
+        }
+        self.time_to_stop = self.queue.qsize()
+        self.queue.put(item)
     
     def get_latest_data(self):
         self.session.commit()
--- socket_builder.py
+++ socket_builder.py
@@ -4,11 +4,12 @@ import json
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from etl.data_loader import DataLoader
 
 from models import ChildPair, ParentPair
 
 class WebSocketThread(threading.Thread):
-    def __init__(self, endpoint, symbol, parent_pair):
+    def __init__(self, endpoint, symbol, parent_pair, data_loader: DataLoader):
         super().__init__()
         self.ws = websocket.WebSocketApp(
             endpoint,
@@ -22,58 +23,60 @@ class WebSocketThread(threading.Thread):
         self.updates = []
 
         # Create SQLite database and tables
-        engine = create_engine('sqlite:///Cryptos.db', echo=False)
+        engine = create_engine('sqlite:///Cryptos.db', echo=False, connect_args={'check_same_thread': False})
         Session = sessionmaker(bind=engine)
         self.session = Session()
-        self.store_init_data()
+        self.data_loader = data_loader
+        self.data_loader.queue_init_data(self.parent_pair, self.symbol)
 
         # Insert parent pair into the parent_pairs table
         # print("my_parent_pair", parent_pair)
         
-    def store_init_data(self):
-        parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==self.parent_pair).first()
-        if not parent_pair_obj:
-            parent_pair_obj = ParentPair(symbol=self.parent_pair)
-            self.session.add(parent_pair_obj)
-            self.session.commit()
-            parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==self.parent_pair).first()
-        self.parent_pair_obj = parent_pair_obj
+    # def store_init_data(self):
+    #     parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==self.parent_pair).first()
+    #     if not parent_pair_obj:
+    #         parent_pair_obj = ParentPair(symbol=self.parent_pair)
+    #         self.session.add(parent_pair_obj)
+    #         self.session.commit()
+    #         parent_pair_obj = self.session.query(ParentPair).filter(ParentPair.symbol==self.parent_pair).first()
+    #     self.parent_pair_obj = parent_pair_obj
         
-        child_pair_obj = self.session.query(ChildPair).filter(
-            (ChildPair.symbol==self.symbol)
-            & (ChildPair.parent_pair_id==parent_pair_obj.id)
-        ).first()
-        if not child_pair_obj:
-            child_pair_obj = ChildPair(
-                symbol=self.symbol,
-                parent_pair_id = parent_pair_obj.id,
-            )
-            self.session.add(child_pair_obj)
-            self.session.commit()
-            child_pair_obj = self.session.query(ChildPair).filter(
-                (ChildPair.symbol==self.symbol)
-                & (ChildPair.parent_pair_id==parent_pair_obj.id)
-            ).first()
-        self.child_pair_obj = child_pair_obj
+    #     child_pair_obj = self.session.query(ChildPair).filter(
+    #         (ChildPair.symbol==self.symbol)
+    #         & (ChildPair.parent_pair_id==parent_pair_obj.id)
+    #     ).first()
+    #     if not child_pair_obj:
+    #         child_pair_obj = ChildPair(
+    #             symbol=self.symbol,
+    #             parent_pair_id = parent_pair_obj.id,
+    #         )
+    #         self.session.add(child_pair_obj)
+    #         self.session.commit()
+    #         child_pair_obj = self.session.query(ChildPair).filter(
+    #             (ChildPair.symbol==self.symbol)
+    #             & (ChildPair.parent_pair_id==parent_pair_obj.id)
+    #         ).first()
+    #     self.child_pair_obj = child_pair_obj
 
     def on_open(self, ws):
         print(f"Connected {self.symbol}")
 
     def on_message(self, ws, message):
         update = json.loads(message)
-        self.store_data(update)
+        self.data_loader.queue_latest_data(self.parent_pair, self.symbol, update)
+        # self.store_data(update)
 
-    def store_data(self, update):
+    # def store_data(self, update):
 
-        # Extract data from the message
-        best_bid_price = float(update['b'])
-        best_ask_price = float(update['a'])
+    #     # Extract data from the message
+    #     best_bid_price = float(update['b'])
+    #     best_ask_price = float(update['a'])
 
-        if self.child_pair_obj:
-            self.child_pair_obj.best_bid_price = best_bid_price
-            self.child_pair_obj.best_ask_price = best_ask_price
-            self.session.add(self.child_pair_obj)
-            self.session.commit()
+    #     if self.child_pair_obj:
+    #         self.child_pair_obj.best_bid_price = best_bid_price
+    #         self.child_pair_obj.best_ask_price = best_ask_price
+    #         self.session.add(self.child_pair_obj)
+    #         self.session.commit()
 
 
     def on_error(self, ws, error):
